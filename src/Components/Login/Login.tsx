12ded28a041a363872c03581c958f8b73a545aed
Kirill Shevchenko <stranger.net3@gmail.com>
2021-06-16 19:47:34 +0400

    Login page UI

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -5,9 +5,7 @@ import * as Yup from 'yup';
 
 const LoginValidateSchema = Yup.object().shape({
   email: Yup.string().email('Incorrect email').required('Field is required'),
-  password: Yup.string()
-    .min(8, 'Min length is 8')
-    .required('Field is required'),
+  password: Yup.string().required('Field is required'),
 });
 
 interface LoginFormValues {
@@ -16,31 +14,80 @@ interface LoginFormValues {
   rememberMe: boolean;
 }
 
-const Login: React.FC = props => {
+const Login: React.FC = () => {
   return (
     <div className={css.login}>
       <Formik
-        initialValues={{ email: '', password: '', rememberMe: false }}
+        initialValues={{
+          email: '',
+          password: '',
+          rememberMe: false,
+        }}
         validationSchema={LoginValidateSchema}
         onSubmit={(values: LoginFormValues) => {
           console.log(values);
         }}
       >
-        {({ errors, touched }) => (
+        {({ errors, touched, setFieldValue, setTouched }) => (
           <Form>
-            <Field type='email' name='email' placeholder='Email' id='email' />
-            {errors.password && touched.password ? (
-              <div>{errors.password}</div>
-            ) : null}
-            <Field
-              type='password'
-              name='password'
-              placeholder='Password'
-              id='password'
-            />
-            <Field type='checkbox' name='rememberMe' id='rememberMe' />
-            <label htmlFor='rememberMe'>Remember me</label>
-            <button type='submit'>Submit</button>
+            <div className={css.title}>
+              <span>Login</span>
+            </div>
+            <div className={css.inner}>
+              {errors.email && touched.email ? (
+                <label htmlFor='password' className={css.error}>
+                  {errors.email}
+                </label>
+              ) : null}
+              <Field
+                type='email'
+                name='email'
+                placeholder='Email'
+                id='email'
+                className={css.email}
+              />
+            </div>
+
+            <div className={css.inner}>
+              {errors.password && touched.password ? (
+                <label htmlFor='email' className={css.error}>
+                  {errors.password}
+                </label>
+              ) : null}
+
+              <Field
+                type='password'
+                name='password'
+                placeholder='Password'
+                id='password'
+                className={css.password}
+              />
+            </div>
+
+            <div className={css.row}>
+              <Field
+                type='checkbox'
+                name='rememberMe'
+                id='rememberMe'
+                className={css.rememberMe}
+              />
+              <label htmlFor='rememberMe'>Remember me</label>
+            </div>
+
+            <button type='submit' className={css.submitBtn}>
+              LOGIN
+            </button>
+
+            <div
+              className={css.useTempAcc}
+              onClick={() => {
+                setFieldValue('email', 'free@samuraijs.com');
+                setFieldValue('password', 'free');
+                setTouched({ email: false, password: false });
+              }}
+            >
+              <span>Use template account</span>
+            </div>
           </Form>
         )}
       </Formik>
