c5595a1e30fce2430d562037c3daff81529dc8d1
Kirill Shevchenko <stranger.net3@gmail.com>
2021-09-18 16:22:16 +0400

    Refactor store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers/rootReducer';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 
-//@ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = configureStore({
+  reducer: rootReducer,
+});
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
-
-export type RootState = ReturnType<typeof rootReducer>;
+export type RootState = ReturnType<typeof store.getState>;
+export type RootDispatch = typeof store.dispatch;
 export type RootReducer = typeof rootReducer;
-
 export type Action<T> = T extends { [key: string]: infer U } ? U : never;
 
 export default store;
